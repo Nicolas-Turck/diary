30c14e8b9aea165b4e43b9b5abf82ff028522c0e
eder <eder@MacBook-Pro-de-eder.local>
2020-02-03 18:27:14 +0100

    add 3 files in model folder

---
+++ main.py
@@ -0,0 +1,8 @@
+from model.connection import *
+
+
+if __name__=='__main__':
+
+    test = Connection()
+    test.initialize_connection()
+    test.close_connection()
---
+++ model/connection.py
@@ -0,0 +1,29 @@
+import psycopg2
+import psycopg2.extras
+
+class Connection():
+    """class for connect and close the database
+    and addinf user port and database in attributes of classe"""
+    USER = "eder"
+    PORT = "5432"
+    DATABASE = "diary"
+
+    def __init__(self):
+        """"""
+        self.connection = None
+        self.cursor = None
+
+    def initialize_connection(self):
+        """method for initialyse the connection to the database """
+        try:
+            self.connection = psycopg2.connect(user = Connection.USER, port = Connection.PORT, database = Connection.DATABASE)
+            self.cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            print("test connection to bdd is ok")
+        except (Exception, psycopg2.Error) as error:
+            print("error no connecting PostgreSQL", error)
+
+    def close_connection(self):
+        """method for closed the database"""
+        if(self.connection):
+            self.cursor.close()
+            self.connection.close()
